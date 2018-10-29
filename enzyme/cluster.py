1048658fcb8dd3db56c852cf19841695f5afab42
neilswainston <neil.swainston@manchester.ac.uk>
2018-10-29 15:57:00 +0000

    Updated clustering code.

--- enzyme/cluster.py
+++ enzyme/cluster.py
@@ -13,28 +13,22 @@ from synbiochem.utils import seq_utils
 
 def get_positions(id_seqs, ref_seq_id, target_seq_id, positions):
     '''Get aligned positions relative to reference sequence.'''
-    target_positions = []
-
     clustal_file = 'clustal.fasta'
     seq_utils.do_clustal(id_seqs, result_file=clustal_file)
 
-    clustered_seqs = seq_utils.read_fasta(clustal_file)
+    cluster_seqs = seq_utils.read_fasta(clustal_file)
 
-    ref_seq = clustered_seqs[ref_seq_id]
-    ref_seq_aln_pos_map, _ = _get_pos_map(ref_seq)
+    ref_seq = cluster_seqs[ref_seq_id]
+    ref_seq_aln, _ = _get_pos_map(ref_seq)
 
-    trgt_seq = clustered_seqs[target_seq_id]
-    _, trgt_aln_seq_pos_map = _get_pos_map(trgt_seq)
+    trgt_seq = cluster_seqs[target_seq_id]
+    _, trgt_aln_seq = _get_pos_map(trgt_seq)
 
-    for pos in positions:
-        aln_pos = ref_seq_aln_pos_map[pos]
-        trgt_seq_pos = trgt_aln_seq_pos_map[aln_pos]
-
-        print [pos, aln_pos, ref_seq[aln_pos]]
-        print [trgt_seq_pos, aln_pos, trgt_seq[aln_pos]]
-        target_positions.append(trgt_seq_pos)
-
-    return target_positions
+    return [[pos,
+             ref_seq[ref_seq_aln[pos]],
+             trgt_aln_seq[ref_seq_aln[pos]],
+             trgt_seq[ref_seq_aln[pos]]]
+            for pos in positions]
 
 
 def _get_pos_map(seq):
@@ -55,7 +49,8 @@ def _get_pos_map(seq):
 def main(args):
     '''main method.'''
     id_seqs = seq_utils.read_fasta(args[0])
-    get_positions(id_seqs, args[1], args[2], [int(pos) for pos in args[3:]])
+    print get_positions(id_seqs, args[1], args[2],
+                        [int(pos) for pos in args[3:]])
 
 
 if __name__ == '__main__':
