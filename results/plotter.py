940353d89bd5ce4327bb1bb48e2bc1c6c3472940
neilswainston <neil.swainston@manchester.ac.uk>
2019-04-08 15:49:25 +0100

    Updated enzyme plot.

--- results/plotter.py
+++ results/plotter.py
@@ -17,32 +17,41 @@ import seaborn as sns
 
 def plot(filename):
     '''Plot.'''
+    sns.set(style='ticks', palette='pastel')
+
     xls = pd.ExcelFile(filename)
 
     for sheet_name in xls.sheet_names:
-        _, ax = plt.subplots()
+        _boxplot(xls, sheet_name)
 
-        df = pd.read_excel(xls, sheet_name)
-        rep_cols = [col for col in df.columns if col.startswith('Rep')]
-        reps_df = pd.DataFrame([[idx, val]
-                                for idx, vals in df[rep_cols].iterrows()
-                                for val in vals.values],
-                               columns=['idx', 'target conc']).set_index('idx')
 
-        reformat_df = df.drop(rep_cols, axis=1).join(reps_df)
+def _boxplot(xls, sheet_name):
+    '''Box plot.'''
+    _, ax = plt.subplots()
+    ax.grid(True)
 
-        sns.boxplot(x='id', y='target conc',
-                    data=reformat_df,
-                    hue='substrate concentration',
-                    ax=ax)
+    df = pd.read_excel(xls, sheet_name)
+    rep_cols = [col for col in df.columns if col.startswith('Rep')]
+    reps_df = pd.DataFrame([[idx, val]
+                            for idx, vals in df[rep_cols].iterrows()
+                            for val in vals.values],
+                           columns=['idx', 'target conc']).set_index('idx')
 
-        # sns.swarmplot(x='id', y='target conc',
-        #             data=reformat_df,
-        #              hue='substrate concentration',
-        #              color='.25',
-        #              ax=ax)
+    reformat_df = df.drop(rep_cols, axis=1).join(reps_df)
 
-        plt.savefig(filename + '.png')
+    bxplt = sns.boxplot(x='id', y='target conc',
+                        data=reformat_df,
+                        hue='substrate concentration',
+                        ax=ax)
+
+    bxplt.axes.set_title(sheet_name)
+
+    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0,
+                     title='substrate conc.')
+
+    plt.savefig('%s.png' % sheet_name,
+                bbox_extra_artists=(lgd,),
+                bbox_inches='tight')
 
 
 def main(args):
