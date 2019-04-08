f5d6e30a608b1480e905c59061c6f27d21407819
neilswainston <neil.swainston@manchester.ac.uk>
2019-04-08 16:34:39 +0100

    Interim check-in for multiple product plots.

--- results/plotter.py
+++ results/plotter.py
@@ -22,14 +22,12 @@ def plot(filename):
     xls = pd.ExcelFile(filename)
 
     for sheet_name in xls.sheet_names:
-        _boxplot(xls, sheet_name)
+        df = _get_df(xls, sheet_name)
+        _boxplot(df)
 
 
-def _boxplot(xls, sheet_name):
-    '''Box plot.'''
-    _, ax = plt.subplots()
-    ax.grid(True)
-
+def _get_df(xls, sheet_name):
+    '''Get df.'''
     df = pd.read_excel(xls, sheet_name)
     rep_cols = [col for col in df.columns if col.startswith('Rep')]
     reps_df = pd.DataFrame([[idx, val]
@@ -38,18 +36,30 @@ def _boxplot(xls, sheet_name):
                            columns=['idx', 'target conc']).set_index('idx')
 
     reformat_df = df.drop(rep_cols, axis=1).join(reps_df)
+    reformat_df.name = sheet_name
 
-    bxplt = sns.boxplot(x='id', y='target conc',
-                        data=reformat_df,
-                        hue='substrate concentration',
-                        ax=ax)
+    return reformat_df
 
-    bxplt.axes.set_title(sheet_name)
 
-    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0,
+def _boxplot(df):
+    '''Box plot.'''
+    fig, axs = plt.subplots(ncols=len(df['target'].unique()),
+                            sharey=True)
+
+    for idx, (substrate, group_df) in enumerate(df.groupby('target')):
+        ax = axs[idx]
+        ax.grid(True)
+        ax.set_title(substrate)
+
+        sns.boxplot(x='id', y='target conc',
+                    data=group_df,
+                    hue='substrate concentration',
+                    ax=ax)
+
+    lgd = fig.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0,
                      title='substrate conc.')
 
-    plt.savefig('%s.png' % sheet_name,
+    plt.savefig('%s.png' % df.name,
                 bbox_extra_artists=(lgd,),
                 bbox_inches='tight')
 
